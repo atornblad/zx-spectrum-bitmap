14d0a0b8d4bc66b2d14f9462c0d3a398126c01b5
Anders Tornblad, web developer <anders.tornblad@gmail.com>
2016-04-05 16:57:44 +0200

    Expression evaluation, demos

--- zxbasic.js
+++ zxbasic.js
@@ -6,11 +6,13 @@
     
     var Basic = Spectrum['Basic'] = Spectrum['Basic'] || {};
     
-    var tokensPattern = /([-+]?((\.\d+)|(\d+(\.\d+)?))(E([-+]?)\d+)?)|(TO|STEP)|([a-z]+)|([=,+/*)()])/gi;
+    var tokensPattern = /([-+]?((\.\d+)|(\d+(\.\d+)?))(E([-+]?)\d+)?)|(TO|STEP|THEN)|([a-z]+)|([,;])|([=<>+*\-/]+)/gi;
     var NUMBER_TOKEN = 1,
         RESERVED_WORD_TOKEN = 8,
         IDENTIFIER_TOKEN = 9,
-        OPERATOR_TOKEN = 10;
+        SEPARATOR_TOKEN = 10,
+        OPERATOR_TOKEN = 11,
+        EXPRESSION_TOKEN = 1000;
     
     var IGNORE_ARG = 0,
         NUMBER_ARG = 1,
@@ -48,20 +50,20 @@
             }
         },
         "plot" : {
-            args : [NUMBER_ARG, NUMBER_ARG],
-            func : function(x, y) {
+            args : [NUMBER_ARG, ",", NUMBER_ARG],
+            func : function(x, comma, y) {
                 ZX.Spectrum.currentBitmap.plot(x, y);
             }
         },
         "draw" : {
-            args : [NUMBER_ARG, NUMBER_ARG],
-            func : function(x, y) {
+            args : [NUMBER_ARG, ",", NUMBER_ARG],
+            func : function(x, comma, y) {
                 ZX.Spectrum.currentBitmap.draw(x, y);
             }
         },
         "let" : {
-            args : [IDENTIFIER_ARG, NUMBER_ARG],
-            func : function(identifier, value) {
+            args : [IDENTIFIER_ARG, "=", NUMBER_ARG],
+            func : function(identifier, equals, value) {
                 this.variables[identifier] = value;
             }
         },
@@ -82,14 +84,14 @@
             }
         },
         "poke" : {
-            args : [NUMBER_ARG, NUMBER_ARG],
-            func : function(address, value) {
+            args : [NUMBER_ARG, ", ", NUMBER_ARG],
+            func : function(address, comma, value) {
                 ZX.Spectrum.currentBitmap.poke(address, value);
             }
         },
         "for" : {
-            args : [IDENTIFIER_ARG, NUMBER_ARG, "TO", NUMBER_ARG, "STEP", NUMBER_ARG],
-            func : function(identifier, start, to, target, step, step_value) {
+            args : [IDENTIFIER_ARG, "=", NUMBER_ARG, "TO", NUMBER_ARG, "STEP", NUMBER_ARG],
+            func : function(identifier, equals, start, to, target, step, step_value) {
                 step_value = typeof(step_value) == 'undefined' ? 1 : step_value;
                 this.variables[identifier] = start;
                 return { for_loop : {
@@ -120,6 +122,165 @@
                     return { error : "NEXT without FOR" };
                 }
             }
+        },
+        "rem" : {
+            args : [],
+            func : function() { }
+        }
+    };
+    
+    var groupExpressions = function(tokens, letOrForCommand) {
+        var result = [];
+        
+        var currentExpressionTokens = [];
+        
+        for (var i = 1; i < tokens.length; ++i) {
+            var token = tokens[i];
+            
+            if (letOrForCommand && ( (i == 1 && token.type == IDENTIFIER_TOKEN) || (i == 2 && token.value == '=') ) ) {
+                result.push(token);
+                continue;
+            }
+            
+            if (token.type == SEPARATOR_TOKEN || token.type == RESERVED_WORD_TOKEN) {
+                if (currentExpressionTokens.length == 1) {
+                    result.push(currentExpressionTokens[0]);
+                } else if (currentExpressionTokens.length > 1) {
+                    result.push( {
+                        type : EXPRESSION_TOKEN,
+                        value : currentExpressionTokens
+                    } );
+                }
+                currentExpressionTokens = [];
+                result.push(token);
+            } else {
+                currentExpressionTokens.push(token);
+            }
+        }
+        
+        if (currentExpressionTokens.length == 1) {
+            result.push(currentExpressionTokens[0]);
+        } else if (currentExpressionTokens.length > 1) {
+            result.push( {
+                type : EXPRESSION_TOKEN,
+                value : currentExpressionTokens
+            } );
+        }
+        
+        return result;
+    };
+    
+    var operatorPriorities = {
+        "+" : 1,
+        "-" : 1,
+        "*" : 2,
+        "/" : 2
+    };
+    
+    var getTokenPriority = function(token) {
+        switch (token.type) {
+            case OPERATOR_TOKEN:
+                return operatorPriorities[token.value];
+            default:
+                return 999;
+        }
+    };
+    
+    var makeExpressionTree = function(tokens) {
+        // this : runtime
+        var root = {
+            token : null,  // "return" token
+            priority : 0,
+            left : null,
+            right : null,
+            parent : null
+        };
+        
+        var latestNode = root;
+        
+        var index = 0;
+        
+        while (index < tokens.length) {
+            var current = tokens[index];
+            var currentPriority = getTokenPriority(current);
+            
+            var targetParent = latestNode;
+            
+            while (targetParent.priority >= currentPriority) {
+                targetParent = targetParent.parent;
+            }
+            
+            latestNode = targetParent.right = {
+                token : current,
+                priority : currentPriority,
+                left : targetParent.right,
+                right : null,
+                parent : targetParent
+            };
+            
+            ++index;
+        }
+        
+        return root;
+    };
+    
+    var evaluateExpressionNode = function(node) {
+        if (!node) return 0;
+        
+        if (node.token) {
+            switch (node.token.type) {
+                case OPERATOR_TOKEN:
+                    switch (node.token.value) {
+                        case '+':
+                            return evaluateExpressionNode.call(this, node.left) +
+                                   evaluateExpressionNode.call(this, node.right);
+                        case '-':
+                            return evaluateExpressionNode.call(this, node.left) -
+                                   evaluateExpressionNode.call(this, node.right);
+                        case '*':
+                            return evaluateExpressionNode.call(this, node.left) *
+                                   evaluateExpressionNode.call(this, node.right);
+                        case '/':
+                            return evaluateExpressionNode.call(this, node.left) /
+                                   evaluateExpressionNode.call(this, node.right);
+                    }
+                    break;
+                case IDENTIFIER_TOKEN:
+                    var identifier = node.token.value.toUpperCase();
+                    if (identifier == "RND") {
+                        return Math.random();
+                    } else if (identifier in this.variables) {
+                        return this.variables[identifier];
+                    } else {
+                        return 0;
+                    }
+                case NUMBER_TOKEN:
+                    return parseFloat(node.token.value);
+            }
+        } else {
+            return evaluateExpressionNode.call(this, node.right);
+        }
+    };
+    
+    var evaluateExpression = function(token) {
+        var tree = makeExpressionTree.call(this, token);
+        
+        return evaluateExpressionNode.call(this, tree);
+    };
+    
+    var evaluateNumber = function(token) {
+        // this : runtime
+        switch (token.type) {
+            case NUMBER_TOKEN:
+                return parseFloat(token.value);
+            case IDENTIFIER_TOKEN:
+                if (token.value.toUpperCase() in this.variables) {
+                    return parseFloat(this.variables[token.value.toUpperCase()]);
+                } else {
+                    return 0;
+                }
+            case EXPRESSION_TOKEN:
+                return evaluateExpression.call(this, token.value);
         }
     };
     
@@ -157,37 +318,34 @@
         // TODO: Support expressions like 5 + (x * 3)
         // But for now, keep it simple
         
-        var commandName = tokens[0].value;
-        var command = commands[commandName.toLowerCase()];
+        var commandName = tokens[0].value.toLowerCase();
+        var command = commands[commandName];
         if (!command) {
             return { error : 'Unknown command: ' + commandName };
         } else {
+            var letOrFor = (commandName == 'let' || commandName == 'for');
+            tokens = groupExpressions.call(this, tokens, letOrFor);
+            
             var args = [];
-            for (var i = 1; i < tokens.length; ++i) {
-                if (tokens[i].type != OPERATOR_TOKEN) {
-                    var argValue = tokens[i].value;
-                    
-                    if (command.args[args.length] == NUMBER_ARG) {
-                        if (tokens[i].type == NUMBER_TOKEN) {
-                            args.push(parseFloat(tokens[i].value));
-                        } else if (tokens[i].type == IDENTIFIER_TOKEN) {
-                            args.push(this.variables[tokens[i].value.toUpperCase()]);
-                        }
-                    } else if (command.args[args.length] == IDENTIFIER_ARG) {
-                        if (tokens[i].type == IDENTIFIER_TOKEN) {
-                            args.push(tokens[i].value.toUpperCase());
-                        } else {
-                            throw "Syntax error: " + lineOfCode;
-                        }
-                    } else if (typeof command.args[args.length] == "string") {
-                        if (tokens[i].type == RESERVED_WORD_TOKEN && tokens[i].value.toUpperCase() == command.args[args.length].toUpperCase()) {
-                            args.push(null);
-                        } else {
-                            throw "Syntax error: " + lineOfCode;
-                        }
+            for (var i = 0; i < tokens.length; ++i) {
+                var argValue = tokens[i].value;
+                
+                if (command.args[args.length] == NUMBER_ARG) {
+                    args.push(evaluateNumber.call(this, tokens[i]));
+                } else if (command.args[args.length] == IDENTIFIER_ARG) {
+                    if (tokens[i].type == IDENTIFIER_TOKEN) {
+                        args.push(argValue.toUpperCase());
                     } else {
-                        args.push(tokens[i].value);
+                        throw "Syntax error: " + lineOfCode;
                     }
+                } else if (typeof command.args[args.length] == "string") {
+                    if ((tokens[i].type == RESERVED_WORD_TOKEN || tokens[i].type == SEPARATOR_TOKEN || tokens[i].type == OPERATOR_TOKEN) && argValue.toUpperCase() == command.args[args.length].toUpperCase()) {
+                        args.push(null);
+                    } else {
+                        throw "Syntax error: " + lineOfCode;
+                    }
+                } else {
+                    args.push(argValue);
                 }
             }
             
