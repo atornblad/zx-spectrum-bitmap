ec03ae3707c26b7f6d485e1f221fdf7ce5e5bbc7
Anders Tornblad, web developer <anders.tornblad@gmail.com>
2016-04-05 17:01:36 +0200

    Fixed Bad merge...

--- zxbasic.js
+++ zxbasic.js
@@ -6,14 +6,6 @@
     
     var Basic = Spectrum['Basic'] = Spectrum['Basic'] || {};
     
-<<<<<<< HEAD
-    var tokensPattern = /([-+]?((\.\d+)|(\d+(\.\d+)?))(E([-+]?)\d+)?)|([a-z]+)|([=,+/*)()])/gi;
-    var NUMBER_TOKEN = 1,
-        IDENTIFIER_TOKEN = 8,
-        OPERATOR_TOKEN = 9;
-    
-    var NUMBER_ARG = 1;
-=======
     var tokensPattern = /([-+]?((\.\d+)|(\d+(\.\d+)?))(E([-+]?)\d+)?)|(TO|STEP|THEN)|([a-z]+)|([,;])|([=<>+*\-/]+)/gi;
     var NUMBER_TOKEN = 1,
         RESERVED_WORD_TOKEN = 8,
@@ -25,7 +17,6 @@
     var IGNORE_ARG = 0,
         NUMBER_ARG = 1,
         IDENTIFIER_ARG = 2;
->>>>>>> refs/remotes/origin/master
     
     var commands = {
         "ink" : {
@@ -59,23 +50,12 @@
             }
         },
         "plot" : {
-<<<<<<< HEAD
-            args : [NUMBER_ARG, NUMBER_ARG],
-            func : function(x, y) {
-=======
             args : [NUMBER_ARG, ",", NUMBER_ARG],
             func : function(x, comma, y) {
->>>>>>> refs/remotes/origin/master
                 ZX.Spectrum.currentBitmap.plot(x, y);
             }
         },
         "draw" : {
-<<<<<<< HEAD
-            args : [NUMBER_ARG, NUMBER_ARG],
-            func : function(x, y) {
-                ZX.Spectrum.currentBitmap.draw(x, y);
-            }
-=======
             args : [NUMBER_ARG, ",", NUMBER_ARG],
             func : function(x, comma, y) {
                 ZX.Spectrum.currentBitmap.draw(x, y);
@@ -301,7 +281,6 @@
                 }
             case EXPRESSION_TOKEN:
                 return evaluateExpression.call(this, token.value);
->>>>>>> refs/remotes/origin/master
         }
     };
     
@@ -339,65 +318,6 @@
         // TODO: Support expressions like 5 + (x * 3)
         // But for now, keep it simple
         
-<<<<<<< HEAD
-        var commandName = tokens[0].value;
-        var command = commands[commandName.toLowerCase()];
-        if (!command) {
-            return { error : 'Unknown command: ' + commandName };
-        } else {
-            var args = [];
-            for (var i = 1; i < tokens.length; ++i) {
-                if (tokens[i].type != OPERATOR_TOKEN) {
-                    var argValue = tokens[i].value;
-                    if (command.args[args.length] == NUMBER_ARG) {
-                        args.push(parseFloat(tokens[i].value));
-                    } else {
-                        args.push(tokens[i].value);
-                    }
-                }
-            }
-            command.func.apply(null, args);
-        }
-        
-        console.log(tokens);
-        
-        return null;
-    };
-    
-    Basic['run'] = function run(code) {
-        var len = code.length;
-        
-        var currentIndex = 0;
-        
-        while (currentIndex < len) {
-            var nextLineFeed = code.indexOf('\n', currentIndex);
-            var nextColon = code.indexOf(':', currentIndex);
-            var nextStop;
-            
-            if (nextColon == -1 && nextLineFeed == -1) {
-                nextStop = len;
-            } else if (nextLineFeed == -1) {
-                nextStop = nextColon;
-            } else if (nextColon == -1) {
-                nextStop = nextLineFeed;
-            } else if (nextColon < nextLineFeed) {
-                nextStop = nextColon;
-            } else {
-                nextStop = nextLineFeed;
-            }
-            
-            var statement = code.substr(currentIndex, nextStop - currentIndex);
-            
-            var result = runOneLine(statement.trim());
-            
-            if (result == null) {
-                currentIndex = nextStop + 1;
-            } else if (result.error) {
-                window.alert(result.error);
-                break;
-            }
-        }
-=======
         var commandName = tokens[0].value.toLowerCase();
         var command = commands[commandName];
         if (!command) {
@@ -515,7 +435,6 @@
         };
             
         stepper();
->>>>>>> refs/remotes/origin/master
     };
     
     window['ZX'] = ZX;
